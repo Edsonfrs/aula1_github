df70e5dea602e52df68e719189a576987c2c517e
Edson <edsonfrs@gmail.com>
2019-09-01 12:24:15 -0300

    Acrecentada nova mensagem e eliminação de linhas em branco

--- src/application/Programan.java
+++ src/application/Programan.java
@@ -6,7 +6,7 @@ public class Programan {
 		System.out.println("Bonan Matenon!");
 		System.out.println("Bonan Tagon!");
 		System.out.println("...kaj Bonan Vesperon!");
-
+		System.out.println("Nova mesagxo el Bonan nokton... Bonan Nokton!");
 	}
 
 }

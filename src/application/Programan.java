e39a56e4e4277e65c898ad0f6c087151556e6ae9
Edson <edsonfrs@gmail.com>
2019-09-01 12:21:29 -0300

    Acrecentadas novas mensagens

--- src/application/Programan.java
+++ src/application/Programan.java
@@ -3,7 +3,9 @@ package application;
 public class Programan {
 
 	public static void main(String[] args) {
+		System.out.println("Bonan Matenon!");
 		System.out.println("Bonan Tagon!");
+		System.out.println("...kaj Bonan Vesperon!");
 
 	}
 

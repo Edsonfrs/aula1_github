8f6c926e0b5f3f338889e732f7082a978fd91610
Edson <edsonfrs@gmail.com>
2019-09-01 12:41:03 -0300

    Nova linha...

--- src/application/Programan.java
+++ src/application/Programan.java
@@ -8,6 +8,7 @@ public class Programan {
 		System.out.println("...kaj Bonan Vesperon!");
 		System.out.println("Nova mesagxo el Bonan nokton... Bonan Nokton!");
 		System.out.println("Algo deu errado...");
+		System.out.println("Nova linha...");
 	}
 
 }
